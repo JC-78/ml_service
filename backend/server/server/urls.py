27a4204f235d8f8de39762784ec13d280f834bbd
JC-78 <43752338+JC-78@users.noreply.github.com>
2023-02-16 01:32:13 -0500

    added predict view

--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -13,9 +13,46 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# backend/server/server/urls.py file
+# from django.conf.urls import url, include
+from django.conf.urls import include
+
+from django.urls import re_path
 from django.contrib import admin
 from django.urls import path
 
+from apps.endpoints.urls import urlpatterns as endpoints_urlpatterns
+
+from rest_framework.routers import DefaultRouter
+
+from apps.endpoints.views import EndpointViewSet
+from apps.endpoints.views import MLAlgorithmViewSet
+from apps.endpoints.views import MLRequestViewSet
+from apps.endpoints.views import PredictView # import PredictView
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += endpoints_urlpatterns
+
+
+#
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r"endpoints", EndpointViewSet, basename="endpoints")
+router.register(r"mlalgorithms", MLAlgorithmViewSet, basename="mlalgorithms")
+router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
+
+urlpatterns = [
+    # url(r"^api/v1/", include(router.urls)),
+    path("api/v1/", include(router.urls)),
+    # add predict url
+    path(
+        # "api/v1/(?P<endpoint_name>.+)/predict$", PredictView.as_view(), name="predict"
+        #'api/v1/income_classifier/predict', PredictView.as_view(), name='predict'
+        'api/v1/<str:endpoint_name>/predict', PredictView.as_view(), name='predict'
+    ),
 ]
